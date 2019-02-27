3979152fe2e4549161748a438712d882a09343fd
Stanislav Sănduță <ezio125@gmail.com>
2019-02-28 01:35:43 +0200

    add introduction section and banner

--- Lab02/js/index.js
+++ Lab02/js/index.js
@@ -3,5 +3,14 @@ $(window).resize(function() {
     let viewportWidth = $(window).width();
     if (viewportWidth < 575) {
         $(".btn").removeClass("btn-lg").addClass("btn-sm");
+    } else {
+        $(".btn").removeClass("btn-sm").addClass("btn-lg");
     }
 });
+
+$(document).ready(function() {
+    $('.carousel').carousel({
+        interval: 5000
+    });
+});
+
